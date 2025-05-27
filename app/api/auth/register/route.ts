a0c3aadafc88ea6560f1bea8753abc13eaef5129
AbeDevSaga <2000amandanm@gmail.com>
2025-05-27 22:43:20 +0300

    routes added

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,1 +1,14 @@
 //app/api/auth/register/route.ts
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  console.log("request: ", request);
+  // Fetch agent data (replace with your DB/API logic)
+  const agent = { id, name: "John Doe", role: "Support Agent" };
+
+  return NextResponse.json(agent);
+}
---
+++ app/api/candidates/[id]/route.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  console.log("request: ", request);
+  // Fetch agent data (replace with your DB/API logic)
+  const agent = { id, name: "John Doe", role: "Support Agent" };
+
+  return NextResponse.json(agent);
+}
