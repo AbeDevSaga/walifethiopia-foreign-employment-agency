6eb00c6dc31ca01ae500cf2d6edfe12c985d90f4
AbeDevSaga <2000amandanm@gmail.com>
2025-05-29 03:01:59 +0300

    remove bg

--- app/components/dashboard/Navbar.tsx
+++ app/components/dashboard/Navbar.tsx
@@ -14,11 +14,11 @@ function Navbar({ onToggleSidebar }: NavbarProps) {
   const user = useSelector((state: RootState) => state.auth.user);
 
   return (
-    <div className="navbar flex flex-col md:flex-row lg:flex-row items-center justify-between border border-blue-500 lg:py-4 px-2 md:px-6 lg:px-8 space-x-2 space-y-2">
+    <div className="navbar flex flex-col md:flex-row lg:flex-row items-center justify-between lg:py-4 px-2 md:px-6 lg:px-8 space-x-2 space-y-2">
       <div className="w-full lg:w-1/3">
         <SearchBar onToggleSidebar={onToggleSidebar}/>
       </div>
-      <div className="w-full flex items-center justify-between lg:w-auto space-x-2 lg:space-x-7">
+      <div className="w-full hidden sm:flex md:flex items-center justify-between lg:w-auto space-x-2 lg:space-x-7">
         <LanguageOption />
         <Notification />
         <Theme />
