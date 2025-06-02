c5d5dac3bdbd5e108bedcecb67858b5b9d3afa2e
AbeDevSaga <2000amandanm@gmail.com>
2025-06-02 18:20:16 +0300

    editable

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import UserProfileCard from "@/app/components/ui/UserProfileCard";
 import { userProfileExample } from "@/app/constants/types/profileExample";
-import React, { useState } from "react";
+import React from "react";
 
 function Profile() {
-  const [editable, setEditable] = useState(true);
+  // const [editable, setEditable] = useState(true);
+  const editable = true
   return (
     <div className="w-full h-full pb-2 relative mx-auto px-4 overflow-auto scrollbar-hide">
       <UserProfileCard
